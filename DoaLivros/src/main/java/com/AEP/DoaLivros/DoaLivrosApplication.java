8681b7320643ae4313ab6297c6154f3cf06e5251
masscarello <oliveira10mais@hotmail.com>
2026-09-02 20:02:58 -0300

    iniciado!

---
+++ DoaLivros/src/main/java/com/AEP/DoaLivros/DoaLivrosApplication.java
@@ -0,0 +1,13 @@
+package com.AEP.DoaLivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoaLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoaLivrosApplication.class, args);
+	}
+
+}
---
+++ DoaLivros/src/test/java/com/AEP/DoaLivros/DoaLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AEP.DoaLivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoaLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
