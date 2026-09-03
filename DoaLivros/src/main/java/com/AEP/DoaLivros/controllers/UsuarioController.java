ac2f26d3da781fbf004621b95daee4ff62b81f7a
masscarello <oliveira10mais@hotmail.com>
2026-09-03 20:22:59 -0300

    crud completo usuario

--- DoaLivros/src/main/java/com/AEP/DoaLivros/controllers/UsuarioController.java
+++ DoaLivros/src/main/java/com/AEP/DoaLivros/controllers/UsuarioController.java
@@ -1,4 +1,58 @@
 package com.AEP.DoaLivros.controllers;
 
+import com.AEP.DoaLivros.models.UsuarioModel;
+import com.AEP.DoaLivros.services.UsuarioService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+    @PostMapping
+    public ResponseEntity<UsuarioModel> criarUsuario(@RequestBody UsuarioModel usuario) {
+        return ResponseEntity.ok(usuarioService.criarUsuario(usuario));
+    }
+
+    // Listar todos os usuários
+    @GetMapping
+    public ResponseEntity<List<UsuarioModel>> listarUsuarios() {
+        return ResponseEntity.ok(usuarioService.listarUsuarios());
+    }
+
+    // Buscar usuário por ID
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioModel> buscarPorId(@PathVariable String id) {
+        return usuarioService.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // Atualizar usuário
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioModel> atualizarUsuario(
+            @PathVariable String id,
+            @RequestBody UsuarioModel usuario) {
+
+        return ResponseEntity.ok(
+                usuarioService.atualizarUsuario(id, usuario)
+        );
+    }
+
+    // Deletar usuário
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarUsuario(@PathVariable String id) {
+        usuarioService.deletarUsuario(id);
+        return ResponseEntity.noContent().build();
+    }
 }
+
+
+
--- DoaLivros/src/main/java/com/AEP/DoaLivros/services/UsuarioService.java
+++ DoaLivros/src/main/java/com/AEP/DoaLivros/services/UsuarioService.java
@@ -1,4 +1,38 @@
 package com.AEP.DoaLivros.services;
 
+import com.AEP.DoaLivros.models.UsuarioModel;
+import com.AEP.DoaLivros.repositories.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UsuarioService {
+
+    private UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository){
+        this.usuarioRepository = usuarioRepository;
+    }
+
+   public UsuarioModel criarUsuario(UsuarioModel usuarioModel){
+        return usuarioRepository.save(usuarioModel);
+   }
+
+   public List<UsuarioModel> listarUsuarios(){
+        return usuarioRepository.findAll();
+   }
+   public Optional<UsuarioModel> buscarPorId(String id){
+        return usuarioRepository.findById(id);
+   }
+   public void deletarUsuario(String id) {
+       usuarioRepository.deleteById(id);
+   }
+    public UsuarioModel atualizarUsuario(String id, UsuarioModel usuario) {
+        usuario.setId(id);
+        return usuarioRepository.save(usuario);
+    }
+
+
 }
