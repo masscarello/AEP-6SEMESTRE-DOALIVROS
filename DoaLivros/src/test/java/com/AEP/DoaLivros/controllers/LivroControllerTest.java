0c1a0e187a13d66e1712dac670868b667bfdfa4d
masscarello <oliveira10mais@hotmail.com>
2026-09-09 19:29:44 -0300

    110 atualizado

--- DoaLivros/src/test/java/com/AEP/DoaLivros/controllers/LivroControllerTest.java
+++ DoaLivros/src/test/java/com/AEP/DoaLivros/controllers/LivroControllerTest.java
@@ -34,7 +34,7 @@ public class LivroControllerTest {
     @Test
     void deveCadastrarLivroERetornarOk() {
         LivroModel livro = new LivroModel();
-        livro.setTitulo("Machado de Assis");
+        livro.setTggitulo("Machado de Assis");
         when(livroService.cadastrar(livro)).thenReturn(livro);
 
         ResponseEntity<LivroModel> resposta = livroController.cadastrar(livro);
